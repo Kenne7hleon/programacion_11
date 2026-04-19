e9570ab42e2de2570c99457dc123a6db9b48d575
Kenne7hleon <kennethleonvillarreal218@gmail.com>
2026-04-18 20:57:05 -0600

    añadiendo tarea002 al repositorio

---
+++ tareas/tarea002/ejercicio1.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+#include <cctype>
+using namespace std;
+string salida;
+
+void minusculas(string &palabra) {
+    for (int i = 0; i < palabra.length(); i++) {
+        salida += palabra[i];
+        palabra[i] = tolower(palabra[i]);
+    }
+}
+bool es_palindromo(string palabra){
+    string evaluador;
+    for (int i = palabra.length() - 1; i >= 0; i -= 1){
+        evaluador += palabra[i];
+    }
+    if (palabra == evaluador) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+int main() {
+    string palabra;
+    cout << "Ingrese una palabra" << endl;
+    cin >> palabra;
+    minusculas(palabra);
+    if (es_palindromo(palabra) == true) {
+        cout << "\"" << salida << "\"" << " es palindromo" << endl;
+    } else {
+        cout << "\"" << salida << "\"" << " no es palindromo" << endl;
+    }
+    return 0;
+}
